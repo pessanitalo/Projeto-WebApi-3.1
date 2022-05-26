a749cdbd73465b27fedfe58b63891e5f5a3e3436
pessanitalo <italopessan@gmail.com>
2022-05-26 12:00:19 +0044

    Projeto Inicial

---
+++ ListaVeiculosApp/ListaVeiculos/Controllers/VeiculoController.cs
@@ -0,0 +1,84 @@
+﻿using ListaVeiculos.Api.Data;
+using ListaVeiculos.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ListaVeiculos.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+        private readonly IRepository _repository;
+        public VeiculoController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //ok
+        [HttpGet]
+        public async Task<IActionResult> get()
+        {
+            var result = await _repository.Getveiculos();
+            return Ok(result);
+        }
+
+        //ok
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getId(int id)
+        {
+            var veiculo = await _repository.GetVeiculoById(id);
+            if (veiculo == null) return BadRequest("Veículo não encontrado");
+
+            return Ok(veiculo);
+        }
+
+        //Ok
+        [HttpGet("veiculoCliente/{id}")]
+        public IActionResult getVeiculoClienteId(int id)
+        {
+            var veiculo = _repository.getVeiculoClienteId(id);
+            if (veiculo == null) return BadRequest("Veículo não encontrado");
+
+            return Ok(veiculo);
+        }
+        //ok
+        [HttpPost]
+        public IActionResult Post(Veiculo veiculo)
+        {
+            _repository.Add(veiculo);
+
+            if (_repository.SaveChanges())
+            {
+                return Ok(veiculo);
+            }
+            return BadRequest("Veículo não cadastrado");
+        }
+        //ok
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Veiculo veiculo)
+        {
+            var veiculoId = _repository.GetVeiculoById(id);
+            if (veiculoId == null) return BadRequest("Veículo não encontrado");
+
+            _repository.Update(veiculo);
+
+            if (_repository.SaveChanges())
+            {
+                return Ok(veiculo);
+            }
+            return BadRequest("Veiculo não cadastrado");
+        }
+        //ok
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var veiculoId = _repository.GetVeiculoById(id);
+            if (veiculoId == null) return BadRequest("Veículo não encontrado");
+
+            _repository.Delete(veiculoId);
+            _repository.SaveChanges();
+            return Ok("Veiculo removido com sucesso!");
+        }
+    }
+}
---
+++ ListaVeiculosApp/ListaVeiculos/Data/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using ListaVeiculos.Api.Models;
+using System.Threading.Tasks;
+
+namespace ListaVeiculos.Api.Data
+{
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveChanges();
+
+        //clientes
+        Cliente[] GetClientes();
+        Cliente GetClienteVeiculosId(int id);
+        Task<Cliente> GetClienteById(int id);
+
+        //veiculos
+        Task<Veiculo[]> Getveiculos();
+        Task<Veiculo> getVeiculoClienteId(int id);
+        Task<Veiculo> GetVeiculoById(int id);
+
+    }
+}
